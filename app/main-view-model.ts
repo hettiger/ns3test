74535a0ea24b3c970a4a62ae4eb23455adf779ed
Martin Hettiger <martin@hettiger.com>
2017-04-01 13:17:36 +0200

    Implement basic list view

--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -1,39 +1,79 @@
-import {Observable} from 'data/observable';
+import { Observable } from 'data/observable';
+import { ObservableArray } from "tns-core-modules/data/observable-array";
+import { ListView } from "tns-core-modules/ui/list-view";
 
-export class HelloWorldModel extends Observable {
+interface IThread {
+    thumbnail: string;
+    url: string;
+    title: string;
+}
 
-    private _counter: number;
-    private _message: string;
+interface IThreads {
+    data: {
+        children: [
+            {
+                kind: string;
+                data: IThread;
+            }
+        ]
+    }
+}
 
-    constructor() {
+export class MainViewModel extends Observable {
+
+    private _listView: ListView;
+    private _isLoading: boolean = true;
+    private _threads: ObservableArray<IThread>;
+
+    constructor(listView: ListView) {
         super();
 
-        // Initialize default values.
-        this._counter = 42;
-        this.updateMessage();
+        this._listView = listView;
+        this._listView.opacity = 0;
     }
 
-    get message(): string {
-        return this._message;
+    get isLoading(): boolean {
+        return this._isLoading;
     }
-    
-    set message(value: string) {
-        if (this._message !== value) {
-            this._message = value;
-            this.notifyPropertyChange('message', value)
+
+    set isLoading(value: boolean) {
+        if (this.isLoading !== value) {
+            this._isLoading = value;
+            this.notifyPropertyChange("isLoading", value);
         }
     }
 
-    public onTap() {
-        this._counter--;
-        this.updateMessage();
+    get threads(): ObservableArray<IThread> {
+        if ( ! this._threads) {
+            this._threads = new ObservableArray() as ObservableArray<IThread>;
+
+            fetch("https://www.reddit.com/r/nativescript.json")
+                .then((response) => response.json())
+                .then((jsonObject: any) => {
+                    let threads = <IThreads>jsonObject;
+
+                    threads.data.children.forEach((child) => {
+                        let thread: IThread = child.data;
+
+                        if (   thread.thumbnail.length
+                            && thread.thumbnail.match(/^https?:\/\//i)
+                            && thread.url.length
+                            && thread.title.length)
+                        {
+                            this._threads.push(thread)
+                        }
+                    });
+
+                    this.isLoading = false;
+
+                    this._listView.animate({
+                        opacity: 1,
+                        duration: 600
+                    });
+                });
+        }
+
+        return this._threads;
     }
 
-    private updateMessage() {
-        if (this._counter <= 0) {
-            this.message = 'Hoorraaay! You unlocked the NativeScript clicker achievement!';
-        } else {
-            this.message = `${this._counter} taps left`;
-        }
-    }
 }

76b135810ca9e532bafbe431dcdf6914479278e5
Martin Hettiger <martin@hettiger.com>
2017-04-01 23:51:55 +0200

    Add multiple templates to the list

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,11 +1,16 @@
 import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
-import { MainViewModel } from './main-view-model';
+import { IListItem, MainViewModel } from './main-view-model';
 import { ListView } from "tns-core-modules/ui/list-view";
+import { ObservableArray } from "tns-core-modules/data/observable-array";
 
 export function onLoaded(args: EventData) {
     let page = <Page>args.object;
     let listView = <ListView>page.getViewById("listView");
 
     page.bindingContext = new MainViewModel(listView);
+}
+
+export function itemTemplateSelector(item: IListItem, index: number, items: ObservableArray<IListItem>): string {
+    return item.templateKey;
 }
--- app/main-view-model.ts
+++ app/main-view-model.ts
@@ -19,11 +19,36 @@ interface IThreads {
     }
 }
 
+export interface IListItem {
+    templateKey: string;
+    item: IThread;
+}
+
+class ListItem implements IListItem {
+
+    private _templateKey: string;
+    private _item: any;
+
+    constructor(templateKey: string, item: any) {
+        this._templateKey = templateKey;
+        this._item = item;
+    }
+
+    get templateKey(): string {
+        return this._templateKey;
+    }
+
+    get item(): any {
+        return this._item;
+    }
+
+}
+
 export class MainViewModel extends Observable {
 
     private _listView: ListView;
     private _isLoading: boolean = true;
-    private _threads: ObservableArray<IThread>;
+    private _items: ObservableArray<IListItem>;
 
     constructor(listView: ListView) {
         super();
@@ -43,13 +68,18 @@ export class MainViewModel extends Observable {
         }
     }
 
-    get threads(): ObservableArray<IThread> {
-        if ( ! this._threads) {
-            this._threads = new ObservableArray() as ObservableArray<IThread>;
+    private addListItem(templateKey: string, item?: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this._items.push(new ListItem(templateKey, item));
+            resolve();
+        });
+    }
 
-            fetch("https://www.reddit.com/r/nativescript.json")
-                .then((response) => response.json())
-                .then((jsonObject: any) => {
+    private fetchThreads(topic: string, templateKey: string): Promise<any> {
+        return fetch("https://www.reddit.com/r/" + topic + ".json")
+            .then((response) => response.json())
+            .then((jsonObject: any) => {
+                return new Promise((resolve, reject) => {
                     let threads = <IThreads>jsonObject;
 
                     threads.data.children.forEach((child) => {
@@ -60,10 +90,27 @@ export class MainViewModel extends Observable {
                             && thread.url.length
                             && thread.title.length)
                         {
-                            this._threads.push(thread)
+                            this._items.push(new ListItem(templateKey, thread));
                         }
                     });
 
+                    resolve();
+                });
+            })
+            .catch((error) => {
+                return new Promise((resolve, reject) => reject(error));
+            });
+    }
+
+    get items(): ObservableArray<IListItem> {
+        if ( ! this._items) {
+            this._items = new ObservableArray() as ObservableArray<IListItem>;
+
+            this.addListItem("nativescript-header")
+                .then(() => this.fetchThreads("nativescript", "nativescript"))
+                .then(() => this.addListItem("typescript-header"))
+                .then(() => this.fetchThreads("typescript", "typescript"))
+                .then(() => {
                     this.isLoading = false;
 
                     this._listView.animate({
@@ -73,7 +120,7 @@ export class MainViewModel extends Observable {
                 });
         }
 
-        return this._threads;
+        return this._items;
     }
 
 }
